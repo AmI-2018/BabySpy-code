ee654b86bb7e5c0d2694764a0a96815e93f8d164
sdiblasio <s224669studenti.polito.it>
2018-06-14 18:43:11 +0200

    First draft on Zway handler

---
+++ Zway/zway.py
@@ -0,0 +1,43 @@
+import rest
+import time
+
+#base url of the ZWave API
+base_url = 'http://192.168.0.202:8083'
+device_url = base_url + '/ZWaveAPI/Run/devices[{}].instances[{}].commandClasses[{}]'
+#useful command classes
+comm_classes = {"sensor_multi": '49', "sensor_binary": '48'}
+
+
+def connect():
+    """
+    The function initializes the connection to the z-wave controller
+    and get the list of all devices connected to it
+    :return: list of all devices connected to the controller
+    """
+    #user credentials
+    username = ''
+    password = ''
+
+    #get z-wave devices
+    all_devices = rest.send(url=base_url+'/ZWaveAPI/Data/0', auth=(username, password))
+
+    #clean up all_devices and removing the controller
+    all_devices = all_devices['devices']
+    all_devices.pop('1')
+    return all_devices
+
+
+def set_device(all_devices, device_key, command, value):
+    """
+    Turn the selected device on or off
+    :param device_key: the key of the device required
+    :param command_class: which type of interface to act on
+    :param value: true for getting on, false for getting off
+    :return:
+    """
+    if all_devices[device_key] is not None:
+        instances = all_devices[device_key]['instances']
+        for instance in instances:
+            if comm_classes[command] in all_devices[device_key]['instances'][instance]['commandClasses']:
+                #turn it on (255)
+                url_to_call = (device_url + '.Set(255)').format(device_key, instance, comm_classes[command])

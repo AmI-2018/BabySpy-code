4ca7f51c31b4bb0e6b88e52cc5ec31f036bdf1e5
Rishvara <s237194@studenti.polito.it>
2018-06-17 15:42:14 +0200

    this a videoplayer which has a screen saver

--- OMX Player/test.py
+++ OMX Player/test.py
@@ -1,23 +1,32 @@
+#input must be give when the the video to distract is needed
+import time
 from omxplayer import OMXPlayer
 
+a=2
 
-print("CENTRAL_AI Startup - running initial setup")
-print("Starting Central visual front end")
-loop1 = OMXPlayer(r"C:\Users\Kavinda Herath\Downloads\New folder\Music\A.mp4")
-vid1 = OMXPlayer(r"C:\Users\Kavinda Herath\Downloads\New folder\Music\B.MP4")
+print("running initial setup")
+loop1 = OMXPlayer('FILE PATH',args=['--win', '100 100 640 480','--loop'])#ARGS FOR THE CONTROL OF THE WINDOW SIZE
+vid1 = OMXPlayer('FILE PATH',args=['--win', '100 100 480 360'])
 
-loop1.play()
+while a==2:
+    print("Motion detection activated")
+    time.sleep(2)
+    print("Security system functioning")
+    loop1.play()
+    a = input()
+    while a==1:
+        time.sleep(0.1)
+        a = input()
+        if a == 1:
+            loop1.pause()
+            print("Intruder Detected")
+            vid1.play()
+            print("Playing Vid_Name_Hammerstein")
 
-a=input()
-
-if a==1:
-    loop1.pause()
-    print("Intruder Detected")
-    vid1.play()
-    print("Playing Vid_Name_Hammerstein")
+        else:
+            loop1.play()
+            print("Intruder Detected")
+            vid1.pause()
+            print("Playing Vid_Name_Hammerstein")
 
 
-"""except KeyboardInterrupt:
-    print("terminated by user")
-    loop1.quit()
-    vid1.quit()"""

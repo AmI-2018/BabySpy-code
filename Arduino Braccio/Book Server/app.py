ae4ba670199b5a3bf18103cb7f5d7d732849d6d0
Yeshitha <s226587@studenti.polito.it>
2018-06-18 19:30:53 +0200

    Updating Braccio server

--- Arduino Braccio/Book Server/app.py
+++ Arduino Braccio/Book Server/app.py
@@ -1,6 +1,4 @@
-from flask import Flask, render_template
-
-
+from flask import Flask, render_template, redirect, url_for, request
 
 
 app = Flask(__name__)
@@ -9,25 +7,25 @@ app = Flask(__name__)
 @app.route('/')
 def welcome_page():
 
-    return render_template("Welcome Page.html")
+    return render_template('welcome.html')
 
 
 @app.route('/Retrieve')
 def retrieve():
 
-    return render_template("retrieve.html")
+    return render_template('retrieve.html')
 
 
 @app.route('/Return')
 def place():
 
-    return render_template("place.html")
+    return render_template('place.html')
 
 
 @app.route('/Throw')
 def throw():
 
-    return render_template("throw.html")
+    return render_template('throw.html')
 
 
 if __name__ == '__main__':

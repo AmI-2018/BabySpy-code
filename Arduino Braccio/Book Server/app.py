8df690df66e82707f8528a2b975b5725b7a184f8
Yeshitha <s226587@studenti.polito.it>
2018-06-18 19:14:04 +0200

    Adding the server for the Braccio to control its movements

---
+++ Arduino Braccio/Book Server/app.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template
+
+
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def welcome_page():
+
+    return render_template("Welcome Page.html")
+
+
+@app.route('/Retrieve')
+def retrieve():
+
+    return render_template("retrieve.html")
+
+
+@app.route('/Return')
+def place():
+
+    return render_template("place.html")
+
+
+@app.route('/Throw')
+def throw():
+
+    return render_template("throw.html")
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=8080, debug=True)

d9a84f7996386b2d010127eb06d632c9931632a2
Yeshitha <s226587@studenti.polito.it>
2018-06-19 18:14:03 +0200

    Changes to the Braccio server

--- Arduino Braccio/Book Server/app.py
+++ Arduino Braccio/Book Server/app.py
@@ -1,42 +1,57 @@
-from flask import Flask, render_template, redirect, url_for, request, jsonify
+from flask import Flask, render_template, request, jsonify
 
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def welcome_page(name):
-    return render_template('welcome.html', name=name)
+def welcome_page():
+    #name = request.form['username']
+    return render_template("welcome.html")
 
 
-@app.route('/Retrieve', methods=['GET', 'POST'])
+@app.route('/shelf')
 def retrieve():
-    if request.methods == 'POST':
-        return render_template('retrieve.html')
+    if request.form == ['action']:
+        return render_template("retrieve.html")
+    if request.form == ['reaction']:
+        return render_template("return.html")
 
 
-@app.route('/Return', methods=['GET', 'POST'])
-def place():
-    if request.methods == 'POST':
-        return render_template('place.html')
+@app.route('/K')
+def something():
+    return render_template("retrieve.html")
 
 
-@app.route('/Throw', methods=['GET', 'POST'])
+@app.route('/Throw', methods=['GET'])
 def throw():
-    if request.methods == 'POST':
-        return jsonify("ERROR!")
-    if request.methods == 'GET':
+    if request.method == 'POST':
+        error = "Error! Wrong type of method!"
+        return jsonify(error)
+    if request.method == 'GET':
         signal = direction()
         return jsonify(signal)
-    return render_template('throw.html')
+    return render_template("throw.html")
 
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=8080, debug=True)
 
 
-#Function to provide the direction of the throw based on which sensor
+# Function to provide the direction of the throw based on which sensor
 def direction():
-    command = dict()
-    #Don't forget the json data format
+    command = "Command to throw!"
+    # Don't forget the json data format
     return command
+
+
+# Provides the movmements to retrieve an object
+def take():
+    signal = dict()
+    return signal
+
+
+# Provides the movmements to retrieve an object
+def replace():
+    signal = dict()
+    return signal

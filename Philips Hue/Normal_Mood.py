2a65245d40c1cce4f6392e8c0d1c6fd3b09ab84e
POLITO\s227714 <s227714@studenti.polito.it>
2018-06-20 15:21:53 +0200

    start to code Hue for normal conditions

---
+++ Philips Hue/Normal_Mood.py
@@ -0,0 +1,43 @@
+
+"""
+    @author
+    Amir
+"""
+
+
+import rest
+import time
+
+def light():
+
+
+    # the base URL
+    base_url = 'http://192.168.0.201'
+    # if you are using the emulator, probably the base_url will be:
+    # base_url = 'http://localhost:8000'
+
+    username = '5UsL1ptHh6tp2M0yDdJDXJtOG4J9UO1bYbhpz2Cd'
+    # if you are using the emulator, the username is:
+    # username = 'newdeveloper'
+
+    # lights URL
+    lights_url = base_url + '/api/' + username + '/lights/'
+
+    # get the Hue lights
+    all_the_lights = rest.send(url=lights_url)
+
+
+    if type(all_the_lights) is dict:
+                    # iterate over the Hue lights, turn them on with the color loop effect
+                    light = '8'
+                    #for light in all_the_lights:
+                    url_to_call = lights_url + light + '/state'
+                    body = '{ "on" : true, "hue" : "56100" }'
+                    print(color)
+                    # to set the red color
+                    # body = '{ "hue" : 0 }'
+                    rest.send('PUT', url_to_call, body, {'Content-Type': 'application/json'})
+                    
+
+            else:
+                print('Error:', all_the_lights[0]['error'])

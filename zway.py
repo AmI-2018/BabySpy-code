e8ce10c1364e9f0c51548a99432a5efc0ebf3fe6
sdiblasio <s224669@studenti.polito.it>
2018-07-13 16:26:20 +0200

    Added the forcing of the update of sensors

--- zway.py
+++ zway.py
@@ -1,10 +1,12 @@
 import rest
 import time
 
-#base url of the ZWave API
+# base url of the ZWave API
 base_url = 'http://localhost:8083'
 device_url = base_url + '/ZWaveAPI/Run/devices[{}].instances[{}].commandClasses[{}]'
-#useful command classes
+# update url
+update_url = base_url + '/ZAutomation/api/v1/ZWayVDev_zway_{}-0-{}-{}/command/update'
+# useful command classes
 comm_classes = {"sensor_multi": '49', "sensor_binary": '48'}
 # user credentials
 username = 'admin'
@@ -18,22 +20,21 @@ def connect():
     :return: list of all devices connected to the controller, exluded the latter
     """
 
-    #get z-wave devices
+    # get z-wave devices
     all_devices = rest.send(url=base_url + '/ZWaveAPI/Data/0', auth=(username, password))
-    #print(all_devices)
+    # print(all_devices)
 
-    #clean up all_devices and removing the controller
+    # clean up all_devices and removing the controller
     all_devices = all_devices['devices']
     all_devices.pop('1')
-    #print(all_devices)
+    # print(all_devices)
     return all_devices
 
 
 def set_devices(all_devices, value):
     """
-    Turn the selected device on or off
-    :param device_key: the key of the device required
-    :param command: which type of interface to act on
+    Turn the devices on or off
+    :param all_devices: the list of devices (entire list) to be switched
     :param value: true for getting on, false for getting off
     :return:
     """
@@ -43,13 +44,13 @@ def set_devices(all_devices, value):
             if value:
                 for instance in instances:
                     for command_class in all_devices[device_key]['instances'][instance]['commandClasses']:
-                        #turn it on (255)
+                        # turn it on (255)
                         url_to_call = (device_url + '.Set(255)').format(device_key, instance, command_class)
                         rest.send(url=url_to_call, auth=(username, password))
             else:
                 for instance in instances:
                     for command_class in all_devices[device_key]['instances'][instance]['commandClasses']:
-                        #turn it off (0)
+                        # turn it off (0)
                         url_to_call = (device_url + '.Set(0)').format(device_key, instance, command_class)
                         rest.send(url=url_to_call, auth=(username, password))
 
@@ -64,16 +65,20 @@ def get_value(all_devices, device_key, instance):
     if all_devices[device_key]['instances'][instance] is not None:
         data = list()
         if comm_classes["sensor_multi"] in all_devices[device_key]['instances'][instance]['commandClasses']:
+            url_to_update = update_url.format(device_key, comm_classes['sensor-multi'], instance)
+            rest.send(url=url_to_update, auth=(username, password))
             url_to_call = device_url.format(device_key, instance, comm_classes["sensor_multi"])
             response = rest.send(url=url_to_call, auth=(username, password))
             data.append(response['data']['3']['val']['value'])
         if comm_classes["sensor_binary"] in all_devices[device_key]['instances'][instance]['commandClasses']:
+            url_to_update = update_url.format(device_key, comm_classes['sensor-binary'], instance)
+            rest.send(url=url_to_update, auth=(username, password))
             url_to_call = device_url.format(device_key, instance, comm_classes["sensor_binary"])
             response = rest.send(url=url_to_call, auth=(username, password))
             data.append(response['data']['1']['level']['value'])
     else:
         data = None
-    #print(data)
+    # print(data)
     return data
 
 
@@ -88,7 +93,7 @@ def get_values(all_devices, device_key):
         value = get_value(all_devices, device_key, instance)
         if value is not None:
             data.append(value)
-    if len(data)==0:
+    if len(data) == 0:
         data = None
     return data
 
@@ -98,14 +103,14 @@ if __name__ == '__main__':
     for device in devices:
         print("This is the device ID: " + device)
     set_devices(devices, True)
-    for i in range(0,2):
-        time.sleep(40)
-        motion = get_values(devices,2)
-        door_window = get_values(devices,3)
+    for i in range(0, 10):
+        time.sleep(3)
+        motion = get_values(devices, '2')
+        door_window = get_values(devices, '3')
         print(motion)
-        time.sleep(25)
+        time.sleep(1)
         print(door_window)
-    time.sleep(10)
+    time.sleep(2)
     print("Turning devices off")
     set_devices(devices, False)
     print("Ending")

9c2f40e66670624863c3449460e9390f0e02ec38
AmirZeraa <37483071+AmirZeraa@users.noreply.github.com>
2018-07-15 19:11:21 +0200

    adding TV and reading mode

---
+++ Philips_Hue/Reading-Mode.py
@@ -0,0 +1,38 @@
+
+"""
+    @author
+    Amir
+"""
+
+from . import rest
+import time
+
+def reading():
+
+    # the base URL
+    base_url = 'http://192.168.0.201'
+    # if you are using the emulator, probably the base_url will be:
+    # base_url = 'http://localhost:8000'
+
+    username = '5UsL1ptHh6tp2M0yDdJDXJtOG4J9UO1bYbhpz2Cd'
+    # if you are using the emulator, the username is:
+    # username = 'newdeveloper'
+
+    # lights URL
+    lights_url = base_url + '/api/' + username + '/lights/'
+
+    # get the Hue lights
+    all_the_lights = rest.send(url=lights_url)
+
+
+    if type(all_the_lights) is dict:
+        # iterate over the Hue lights, turn them on with the color loop effect
+        light = '9'
+        #for light in all_the_lights:
+        url_to_call = lights_url + light + '/state'
+        body = '{ "on" : true, "hue" : "12750" }'
+        time.sleep(1)
+        rest.send('PUT', url_to_call, body, {'Content-Type': 'application/json'})
+
+    else:
+        print('Error:', all_the_lights[0]['error'])

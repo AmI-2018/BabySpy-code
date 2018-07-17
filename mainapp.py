15d1c9a878977632c892e9455bcd4b1069b211ad
sdiblasio <s224669@studenti.polito.it>
2018-07-17 16:57:05 +0200

    Some work on the main app

--- mainapp.py
+++ mainapp.py
@@ -1,13 +1,28 @@
 import sys
 sys.path.append('/home/pi/Project/BabySpy-code/')
+sys.path.append('/home/pi/Project/BabySpy-server/')
 import zway
 import rest
 import time
+import random
 from Philips_Hue import test_flashing
+import userdb
 
 light_motion_id = '2'
 door_id = '3'
 base_url = ''
+
+
+def distract(intervent, luminosity):
+    luminosity_highlevel = 150
+    possible_actions = ['recording', 'flashing']
+    if luminosity > luminosity_highlevel:
+        possible_actions.remove('flashing')
+    action = possible_actions[random.randint(0, len(possible_actions)-1)]
+    if action == 'recording':
+        # recording
+    else:
+        # flashing
 
 
 def main():
@@ -23,7 +38,7 @@ def main():
     while True:
         time.sleep(10)
         sensor_lm = zway.get_values(sensors, light_motion_id)
-        lminosity = sensor_lm.pop(0)
+        luminosity = sensor_lm.pop(0)
         motion = sensor_lm.pop(0)
         sensor_door = zway.get_values(sensors, door_id)
         drawer_opened = sensor_door.pop(0)
@@ -42,7 +57,7 @@ def main():
                     data = {'event' : intervent}
                     rest.send('POST', url=base_url + '/api/danger', data=data)
                 else:
-                    # distract*intervent, luminosity)
+                    # distract(intervent, luminosity)
                 last_intervent = intervent
         elif not triggered and previous_intervent:
             previous_intervent = False

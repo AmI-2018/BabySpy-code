b846d2d36a57efe639d7e69c20c4130f55435782
AmirZeraa <37483071+AmirZeraa@users.noreply.github.com>
2018-07-17 12:35:09 +0200

    Coding main app

--- Philips_Hue/Main_Program.py
+++ Philips_Hue/Main_Program.py
@@ -11,20 +11,26 @@
 import zway
 import Normal_Mood
 import braccio
+import time
 
 
 
 def main():
-
+    #getting value from the zwave
   data = zway.get_values()
+    #check if the sensre detect the child in a dangrouse situation
   while data[2] == 'true':
+    #if yes start to distract the child by lights
     if data is 'true':
       import test_flashing
       test_flashing.light()
+    #if not turn the lights on in the normal mode
     else:
         Normal_Mood.normal()
         print ("Safe Condition")
 
+    time.sleep(5)
+    #try to distract the child by Braccio
     if data is 'true':
         braccio.read()
     else:
